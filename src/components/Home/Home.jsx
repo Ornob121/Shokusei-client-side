ca1d6f56aaf36bd62474c7494eccd73dac326ba1
Ornob121 <shadmantahmid121@gmail.com>
2023-05-02 13:42:47 +0600

    banner fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../Banner/Banner";
 import home1 from "../../../public/home1.png";
 import home2 from "../../../public/home2.png";
 import home3 from "../../../public/home3.png";
+import chef from "../../../public/chef.png";
 
 const Home = () => {
   return (
@@ -11,11 +12,11 @@ const Home = () => {
         <div className="bg-zinc-100 pt-[100px] pb-[150px] px-24 grid grid-cols-2 gap-16">
           <div className="grid grid-cols-3 gap-5 banner-home">
             <img
-              className="w-auto h-96 relative bottom-10"
-              src={home1}
+              className=" w-auto h-96 relative bottom-10"
+              src={home2}
               alt=""
             />
-            <img className="w-auto h-[425px]" src={home2} alt="" />
+            <img className="w-auto h-[425px]" src={home1} alt="" />
             <img
               className="w-auto h-72 relative bottom-10"
               src={home3}
